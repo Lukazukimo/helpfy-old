dad31233040de3de60349713d26104238cb83d7b
Lukazukimo <fabio.wuhuang@hotmail.com>
2020-02-25 12:55:38 -0300

    Add ProfileInformation on Top Profile

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -3,7 +3,10 @@ import {
 	View,
 	Text,
 	StyleSheet,
-	ScrollView
+	ScrollView,
+	ImageBackground,
+	Image,
+	Dimensions
 } from 'react-native'
 import {
 	Container, Tabs, Tab, TabHeading
@@ -19,17 +22,62 @@ class Profile extends Component {
 		return(
 			<View style={styles.container}>
 				<ScrollView>
-					<View style={styles.profile}>
-						<Text>
-							Profile
-						</Text>
+					<View style={styles.backgroundContainer}>
+						<ImageBackground
+							source={require("../../assets/imgs/fence.jpg")}
+							imageStyle={{ opacity: 0.75, backgroundColor: 'rgba(107, 13, 200, 0.75)' }}
+							style={styles.imageBackground}>
+							<View style={styles.profileFraseContainer}>
+								<View style={styles.profileContainer}>
+									<View style={styles.donationContainer}>
+										<Text style={styles.donation}>200</Text>
+										<Image 
+											source={require("../../assets/imgs/mao.png")}
+											style={styles.donationImage}/>
+									</View>
+									<View style={styles.profile}>
+										<Image 
+											source={require("../../assets/imgs/icon.png")}
+											style={styles.imageProfile}/>
+										<Text style={styles.name} 
+											numberOfLines={2}>
+											Nome Sobrenome Sobrenome Sobrenome Sobrenome 
+											Sobrenome Sobrenome Sobrenome</Text>									
+									</View>
+									<View style={styles.rankingContainer}>
+										<Text style={styles.ranking}>15#</Text>
+										<Image 
+											source={require("../../assets/imgs/trofeu.png")}
+											style={styles.rankingImage}/>
+									</View>
+								</View>
+								<View style={styles.fraseContainer}>
+									<Text style={styles.frase}
+										numberOfLines={3}>
+										Frase do Dia
+										Frase do Dia
+										Frase do Dia
+										Frase do Dia
+										Frase do Dia
+										Frase do Dia
+										Frase do Dia\Frase do Dia
+										Frase do Dia
+										Frase do Dia
+										Frase do Dia
+										Frase do Dia
+										Frase do Dia
+										Frase do Dia
+									</Text>
+								</View>
+							</View>
+						</ImageBackground>
 					</View>				
 					<Container>
 						<View style={styles.conteudo}>
 							<Tabs>
 								<Tab heading={
 									<TabHeading style={{ backgroundColor : 'red' }}>
-										<Icon name='user-check' size={26}/>										
+										<Icon name='user-check' size={26}/>					
 									</TabHeading>} >
 										<ProfileInformation />
 								</Tab>
@@ -58,16 +106,106 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1
 	},
-	profile: {
+	backgroundContainer: {
 		flex: 1,
-		backgroundColor: 'blue',
-		height: 200
-		
+		// backgroundColor: 'blue',
+		height: 280,		
 	},
+	imageBackground: {
+        width: undefined, 
+        padding: 16,
+		paddingTop: 30,
+		height: 280,
+		justifyContent: 'space-around',
+		alignItems: 'center',				
+	},
+	profileFraseContainer:{		
+		// backgroundColor: 'orange'
+	},
+    imageProfile: {
+        width: 120,
+        height: 120,
+        borderRadius: 60,
+        borderWidth: 3,
+		borderColor: '#993399',
+		marginTop: 18
+	},
+	profileContainer: {	
+		flex: 5,	
+		flexDirection: 'row'
+	},
+	profile: {		
+		flex: 3,
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+	donationContainer: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		paddingTop: 75,	
+		// backgroundColor: 'blue',
+	},
+	rankingContainer:{
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		paddingTop: 85,	
+		// backgroundColor: 'green',
+	},
+	donation:{
+		fontSize: 32,		
+		fontWeight: 'bold',
+		// color: '#fff'
+		color: '#993399',
+	},
+	ranking: {
+		fontSize: 32,		
+		fontWeight: 'bold',
+		// color: '#fff'
+		color: '#993399',
+	},
+	donationImage: {
+		width: 50,
+		height: 40,	
+		resizeMode: 'contain'
+	},
+	rankingImage:{	
+		width: 50,
+		height: 40,	
+		resizeMode: 'contain'	
+	},
+	name: {
+        // color: '#fff',
+        color: '#993399',
+        fontSize: 20,
+        fontWeight: '800',
+		fontWeight: 'bold',
+		marginVertical: 8,		
+		textAlignVertical: "center",
+		textAlign: "center",
+	},
+	fraseContainer: {		
+		flex: 1,
+		width: Dimensions.get('window').width,
+		marginTop: 5,
+		// backgroundColor: 'pink',
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+    frase: {
+        color: 'rgba(255, 255, 255, 0.8)',        
+        fontSize: 15,
+		marginRight: 4,
+		fontWeight: 'bold',
+		fontStyle: "italic",
+		textAlignVertical: "center",
+		textAlign: "center",
+    },
 	conteudo: {
 		flex: 1,
 		backgroundColor: 'red'
-	}
+	},
 })
 
 export default Profile
